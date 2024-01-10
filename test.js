b9af19d25d4d6d768926c02c4469dfde8d719f72
Yuejun Guo <yuejun.guoo@gmail.com>
2024-01-10 15:14:56 +0100

    Update test.js

--- test.js
+++ test.js
@@ -5,23 +5,6 @@ function displayMessage(message) {
   document.getElementById('output').innerHTML = '<p>' + message + '</p>';
 }
 
-// Example usage
-var userInput = '<img src=x onerror=alert("XSS Vulnerability")>';
-displayMessage(userInput);
-
-// This is a simple JavaScript code with an intentional SQL Injection vulnerability
-
-function fetchData(userInput) {
-  // Vulnerable code: This code directly inserts user input into an SQL query
-  var query = "SELECT * FROM users WHERE username = '" + userInput + "';";
-  
-  // This is a simulated database query, do not use this in a real application
-  // It's just for demonstration purposes
-  var result = executeQuery(query);
-
-  return result;
-}
-
 // Simulated database query function
 function executeQuery(query) {
   // In a real application, this function would interact with a database
@@ -34,8 +17,3 @@ function executeQuery(query) {
   // Simulated response, do not use this in a real application
   return fakeDatabase[query];
 }
-
-// Example usage
-var userInput = "admin'; --";
-var queryResult = fetchData(userInput);
-console.log(queryResult);
