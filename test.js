e364d236f30bd667d46ef92de5c4c86d62be2291
Yuejun Guo <yuejun.guoo@gmail.com>
2024-01-22 15:48:10 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,108 @@
-// This is a simple JavaScript code with an intentional XSS vulnerability
+function writeCommentDate(comment, dateDiv)
+	{
+		dateDiv.innerText = '';
+		var ts = new Date(comment.modifiedDate);
+		var str = editorUi.timeSince(ts);
+		if (str == null)
+		{
+			str = mxResources.get('lessThanAMinute');
+		}
+		mxUtils.write(dateDiv, mxResources.get('timeAgo', [str], '{1} ago'));
+		dateDiv.setAttribute('title', ts.toLocaleDateString() + ' ' +
+				ts.toLocaleTimeString());
+	};
 
-function displayMessage(message) {
-  // Vulnerable code: This line does not properly sanitize the user input
-  document.getElementById('output').innerHTML = '<p>' + message + '</p>';
+App.prototype.updateButtonContainer = function()
+    {
+    	appUpdateButtonContainer.apply(this, arguments);
+    	if (this.shareButton != null)
+		{
+    		this.shareButton.style.display = 'none';
+		}
+    };
+
+
+function isArrayBuffer(obj) {
+	return toString.call(obj) === "[object ArrayBuffer]";
 }
 
-// Simulated database query function
-function executeQuery(query) {
-  // In a real application, this function would interact with a database
-  // Here, we'll just simulate a response for testing purposes
-  var fakeDatabase = {
-    'admin': 'Access granted!',
-    'user': 'Normal user access'
-  };
 
-  // Simulated response, do not use this in a real application
-  return fakeDatabase[query];
+EditorUi.prototype.restoreScrollState = function(state)
+	{
+		var s = this.editor.graph.view.scale;
+		var t = this.editor.graph.view.translate;
+		this.diagramContainer.scrollLeft = state.x + (t.x - state.tx) * s;
+		this.diagramContainer.scrollTop = state.y + (t.y - state.ty) * s;
+	};
+
+$scope.refresh = function() {
+      growl.success('Retrieving node ' + _.escape($scope.foreignId) + ' from requisition ' + _.escape($scope.foreignSource) + '...');
+      RequisitionsService.getNode($scope.foreignSource, $scope.foreignId).then(
+        function(node) {  
+          $scope.node = node;
+        },
+        $scope.errorHandler
+      );
+    };
+
+
+function selectedFilesRail(inputFileID) {
+    var fileobj = [];
+    if (inputFileID && inputFileID != '') {
+        setFilesRail(document.getElementById(inputFileID).files);
+    }
 }
+
+
+Git.prototype.getRemotes = function (verbose, then) {
+   return this._runTask(getRemotesTask(verbose === true), trailingFunctionArgument(arguments));
+};
+
+Runnable.prototype.timeout = function(ms){
+  if (0 == arguments.length) return this._timeout;
+  if ('string' == typeof ms) ms = milliseconds(ms);
+  debug('timeout %d', ms);
+  this._timeout = ms;
+  if (this.timer) this.resetTimeout();
+  return this;
+};
+
+Mocha.prototype.grep = function(re) {
+  this.options.grep = 'string' == typeof re
+    ? new RegExp(utils.escapeRegexp(re))
+    : re;
+  return this;
+};
+
+GraphViewer.prototype.updateTitle = function(title)
+{
+	title = title || '';
+	if (this.showTitleAsTooltip && this.graph != null && this.graph.container != null)
+	{
+		this.graph.container.setAttribute('title', title);
+    }
+	if (this.filename != null)
+	{
+		this.filename.innerText = '';
+		mxUtils.write(this.filename, title);
+		this.filename.setAttribute('title', title);
+	}
+};
+
+$scope.delete = function(foreignSource) {
+      bootbox.confirm('Are you sure you want to remove the requisition ' + foreignSource + '?', function(ok) {
+        if (ok) {
+          RequisitionsService.startTiming();
+          RequisitionsService.deleteRequisition(foreignSource).then(
+            function() {  
+              growl.success('The requisition ' + foreignSource + ' has been deleted.');
+            },
+            $scope.errorHandler
+          );
+        }
+      });
+    };
+
+Git.prototype.binaryCatFile = function() {
+   return this._catFile('buffer', arguments);
+};
